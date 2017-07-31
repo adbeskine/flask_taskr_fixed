8659bae30e6504663e3b31938343cf2c9c5dc593
Alexander Beskine <a.d.beskine@outlook.com>
2017-07-31 14:28:33 +0100

    revert to before flask restful messup

--- Flask_taskr/api/views.py
+++ Flask_taskr/api/views.py
@@ -1,12 +1,16 @@
 from functools import wraps
 from flask import flash, redirect, jsonify, \
     session, url_for, Blueprint, make_response
-from flask_restful import Api, Resource, abort
-from flask_taskr import db
+
+from Flask_taskr import db
 from Flask_taskr.models import Task
 
 
+################
+#### config ####
+################
 
+api_blueprint = Blueprint('api', __name__)
 
 
 ##########################
@@ -33,45 +37,33 @@ def closed_tasks():
     return db.session.query(Task).filter_by(
         status='0').order_by(Task.due_date.asc())
 
-def abort_if_task_not_exist(task_id):
-	if task_id not in db.session.query(Task).filter_by(task_id=task_id.first):
-			abort(404, message = "error: task does not exist")
 
 ################
 #### routes ####
 ################
 
-# @api_blueprint.route('/api/v1/tasks/')
+@api_blueprint.route('/api/v1/tasks/')
+def api_tasks():
+    results = db.session.query(Task).limit(10).offset(0).all()
+    json_results = []
+    for result in results:
+        data = {
+            'task_id': result.task_id,
+            'task name': result.name,
+            'due date': str(result.due_date),
+            'priority': result.priority,
+            'posted date': str(result.posted_date),
+            'status': result.status,
+            'user id': result.user_id
+            }
+        json_results.append(data)
+    return jsonify(items=json_results)
 
-
-# @api_blueprint.route('/api/v1/tasks/<int:task_id>')
-
-
-class Get_all_tasks(Resource):
-	def get_all_tasks(self):
-		results = db.session.query(Task).limit(10).offset(0).all()
-		json_results = []
-		for result in results:
-			data = {
-			'task_id': result.task_id,
-			'task name': result.name,
-			'due date': str(result.due_date),
-			'priority': result.priority,
-			'posted date': str(result.posted_date),
-			'status': result.status,
-			'user id': result.user_id
-			}
-			json_results.append(data)
-		return jsonify(items=json_results)
-
-   
-
-class get_individual_task(resource):
-	def get_individual_task(self, task_id):
-		result = db.session.query(Task).filter_by(task_id=task_id).first()
-		abort_if_task_not_exist(task_id)
-		if result:
-			json_result = {
+@api_blueprint.route('/api/v1/tasks/<int:task_id>')
+def task(task_id):
+	result = db.session.query(Task).filter_by(task_id=task_id).first()
+	if result:
+		json_result = {
 			'task_id':result.task_id,
 			'task name': result.name,
 			'due_date':str(result.due_date),
@@ -79,9 +71,11 @@ class get_individual_task(resource):
 			'posted date':str(result.posted_date),
 			'status':result.status,
 			'user id':result.user_id
-			}
+		}
 		code = 200
 		return make_response(jsonify(json_result), code)
-
-
-		
+	else:
+		result = {"error": "Element does not exist"}
+		code = 404
+		return make_response(jsonify(result), code)
+	
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from Flask_taskr import app
 import os
 
-# port = int(os.environ.get('PORT', 5000))
-# app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
 
-app.run(debug=True)
+# app.run(debug=True)
