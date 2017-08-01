95ff2acb43cebc8a46482da72b6e9ede7c0e952d
Alexander Beskine <a.d.beskine@outlook.com>
2017-08-01 13:46:05 +0100

    turned off debug

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from Flask_taskr import app
 import os
 
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
+app.run(host='0.0.0.0', port=port)
 
 # app.run(debug=True)
